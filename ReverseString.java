5455f04f0d255e0c3ce0a22bb9b77cd5cdc30ebd
Mo. Rehan Khan <rehanxtech404@gmail.com>
2026-02-10 19:36:55 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+public class ReverseString{
+public static void main(String[] args){
+String str="Java is a programming language and it is easy to learn";
+StringBuffer sb=new StringBuffer(str);
+sb.reverse();
+String st=sb.toString();
+System.out.println(st);
+                
+
+
+
+
+}
+
+
+
+}
