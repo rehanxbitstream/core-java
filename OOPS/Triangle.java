7661bf8c8b71e756c566341352c64a8b8003ada3
Mo. Rehan Khan <rehanxtech404@gmail.com>
2026-02-13 21:47:16 +0530

    Add files via upload

---
+++ OOPS/Triangle.java
@@ -0,0 +1,59 @@
+public class Triangle{
+private int borderWidth = 0;
+private String color = null;
+private int base = 0;
+private int height = 0;
+
+public void setBorderWidth(int borderWidth){
+this.borderWidth=borderWidth;
+}
+
+public void setColor(String color){
+this.color=color;
+}
+
+public void setHeight(int height){
+this.height=height;
+}
+
+public void setbase(int base){
+this.base=base;
+}
+
+public int getBorderWidth(){
+return borderWidth;
+}
+
+public String getColor(){
+return color;
+}
+
+public int getHeight(){
+return height;
+}
+
+public int getBase(){
+return base;
+}
+public double area(){
+return 0.5*base*height;
+}
+
+public static void main(String[] args){
+Triangle t=new Triangle();
+t.setBorderWidth(34);
+t.setColor("Green");
+t.setHeight(2+3);
+t.setbase(7);
+double area=t.area();
+System.out.println("Area of triangle="+area);
+
+
+}
+
+
+
+
+
+
+}
