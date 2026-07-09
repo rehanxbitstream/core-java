57f189dda48e62ff6c92cf0b8867873c85ef06a9
Mo. Rehan Khan <rehanxtech404@gmail.com>
2026-07-09 18:36:36 +0530

    Implement Singleton pattern in SingletonClass

--- Design Pattern/SingletonClass.java
+++ Design Pattern/SingletonClass.java
@@ -1,1 +1,14 @@
-
+public class SingletonClass{
+  
+private static SingletonClass s=null;
+private SingletonClass(){
+System.out.println("This is singleton class");
+}
+  
+public static SingletonClass getInstance(){
+if(s==null){
+s=new SingletonClass();
+}
+return s;
+}
+}
